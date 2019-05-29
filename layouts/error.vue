239086d7c0c7bc8f03d4f3634863f25ce8f8841e
PhilomathSaeed <philomath.saeed@gmail.com>
2019-05-29 20:03:19 +0600

    Cleaned up a little bit, created a light layout and error layout

---
+++ layouts/error.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h1>We are sorry, an unknown error has occured.</h1>
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -10,9 +10,56 @@
       md6
     >
       <div class="text-xs-center">
-        <logo />
-        <vuetify-logo />
+        <h2>Here goes logos</h2>
+        <input type="checkbox" id="checkbox" v-model="checked" @change="layoutChoice">
+        <label for="checkbox">{{ checked ? "Light" : "Dark" }}</label>
       </div>
+      <v-card>
+        <v-card-title class="headline">Welcome to the Vuetify + Nuxt.js template</v-card-title>
+        <v-card-text>
+          <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
+          <p>For more information on Vuetify, check out the <a
+            href="https://vuetifyjs.com"
+            target="_blank"
+          >documentation</a>.</p>
+          <p>If you have questions, please join the official <a
+            href="https://chat.vuetifyjs.com/"
+            target="_blank"
+            title="chat"
+          >discord</a>.</p>
+          <p>Find a bug? Report it on the github <a
+            href="https://github.com/vuetifyjs/vuetify/issues"
+            target="_blank"
+            title="contribute"
+          >issue board</a>.</p>
+          <p>Thank you for developing with Vuetify and I look forward to bringing more exciting features in the future.</p>
+          <div class="text-xs-right">
+            <em><small>&mdash; John Leider</small></em>
+          </div>
+          <hr class="my-3">
+          <a
+            href="https://nuxtjs.org/"
+            target="_blank"
+          >Nuxt Documentation</a>
+          <br>
+          <a
+            href="https://github.com/nuxt/nuxt.js"
+            target="_blank"
+          >Nuxt GitHub</a>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer />
+          <v-btn
+            color="primary"
+            flat
+            nuxt
+            to="/inspire"
+          >Continue</v-btn>
+        </v-card-actions>
+      </v-card>
+
+      <div class="ma-5"></div>
+
       <v-card>
         <v-card-title class="headline">Welcome to the Vuetify + Nuxt.js template</v-card-title>
         <v-card-text>
@@ -61,13 +108,13 @@
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-import VuetifyLogo from '~/components/VuetifyLogo.vue'
-
 export default {
-  components: {
-    Logo,
-    VuetifyLogo
-  }
+  data() {
+    return {
+      checked: false
+    }
+  },
+
+  layout: this.checked ? "default-light" : "default"
 }
 </script>
